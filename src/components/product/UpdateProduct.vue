ab2fae059315f95a048ddd15df69ea2046f28f82
zhanmian <zhanmian@yeah.net>
2019-03-02 23:31:14 +0800

    Initial commit

---
+++ src/components/product/UpdateProduct.vue
@@ -0,0 +1,15 @@
+<template>
+  <product-detail :is-edit='true'></product-detail>
+</template>
+
+<script>
+  import ProductDetail from './ProductDetail'
+  export default {
+    name: 'updateProduct',
+    components: { ProductDetail }
+  }
+</script>
+
+<style scoped>
+
+</style>
